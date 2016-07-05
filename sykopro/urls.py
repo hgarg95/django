0320c211ac3c1cf1faa456850132def82aa3d248
himanshu <aman1998garg@gmail.com>
2016-07-05 13:33:52 +0530

    Commit

--- sykopro/urls.py
+++ sykopro/urls.py
@@ -21,17 +21,17 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    # url(r'^$', 'project.views.home', name='home'),
-    # url(r'^home/$', 'project.views.home', name='home'),
-    # url(r'^Otp_app/$', 'project.views.Otp_app', name='Otp_app'),
-    # url(r'^signup_app/$', 'project.views.signup_app', name='signup_app'),
-    # url(r'^admin_login_app/$', 'project.views.admin_login_app', name='admin_login_app'),    
-    # url(r'^user_login_app/$', 'project.views.user_login_app', name='user_login_app'),
-    # url(r'^forgot_password/$', 'project.views.forgot_password', name='forgot_password'),
-    # url(r'^update_password_user/$', 'project.views.update_password_user', name='update_password_user'),    
-    # url(r'^update/$', 'project.views.update', name='update'),
-    # url(r'^update_check/$', 'project.views.update_check', name='update_check'),
-    # url(r'^Save_address/$', 'project.views.Save_address', name='Save_address'),
+    url(r'^$', 'project.views.home', name='home'),
+    url(r'^home/$', 'project.views.home', name='home'),
+    url(r'^Otp_app/$', 'project.views.Otp_app', name='Otp_app'),
+    url(r'^signup_app/$', 'project.views.signup_app', name='signup_app'),
+    url(r'^admin_login_app/$', 'project.views.admin_login_app', name='admin_login_app'),    
+    url(r'^user_login_app/$', 'project.views.user_login_app', name='user_login_app'),
+    url(r'^forgot_password/$', 'project.views.forgot_password', name='forgot_password'),
+    url(r'^update_password_user/$', 'project.views.update_password_user', name='update_password_user'),    
+    url(r'^update/$', 'project.views.update', name='update'),
+    url(r'^update_check/$', 'project.views.update_check', name='update_check'),
+    url(r'^Save_address/$', 'project.views.Save_address', name='Save_address'),
 
 
 
