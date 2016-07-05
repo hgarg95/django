8f42ec65148c02376fde9f492cae0aab8b70079e
himanshu <aman1998garg@gmail.com>
2016-07-05 21:34:56 +0530

    Commit

--- project/views.py
+++ project/views.py
@@ -292,9 +292,7 @@ def address_list(request):
 		if query:		
 			for num in range(1,5):
 				for instance in query:
-					q = [instance.address1,instance.address2,instance.address3,instance.address4]
-					if str(q[num-1]):
-						d['address'] = instance.address1
+					d['address'] = instance.address1
 					dlist.append(d.copy())
 			return HttpResponse(json.dumps(dlist))
 		else:
